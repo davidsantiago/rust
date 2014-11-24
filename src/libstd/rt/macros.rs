00ab8473a5eef2606a4f02e6aa8931b572544da6
Aaron Turon <aturon@mozilla.com>
2014-11-23 19:21:17 -0800

    libs: merge librustrt into libstd
    This commit merges the `rustrt` crate into `std`, undoing part of the
    facade. This merger continues the paring down of the runtime system.
    
    Code relying on the public API of `rustrt` will break; some of this API
    is now available through `std::rt`, but is likely to change and/or be
    removed very soon.
    
    [breaking-change]

--- src/libstd/rt/macros.rs
+++ src/libstd/rt/macros.rs
@@ -17,7 +17,7 @@
 
 macro_rules! rterrln (
     ($fmt:expr $($arg:tt)*) => ( {
-        format_args!(::util::dumb_print, concat!($fmt, "\n") $($arg)*)
+        format_args!(::rt::util::dumb_print, concat!($fmt, "\n") $($arg)*)
     } )
 )
 
@@ -32,7 +32,7 @@ macro_rules! rtdebug (
 
 macro_rules! rtassert (
     ( $arg:expr ) => ( {
-        if ::util::ENFORCE_SANITY {
+        if ::rt::util::ENFORCE_SANITY {
             if !$arg {
                 rtabort!(" assertion failed: {}", stringify!($arg));
             }
@@ -42,5 +42,5 @@ macro_rules! rtassert (
 
 
 macro_rules! rtabort (
-    ($($arg:tt)*) => (format_args!(::util::abort, $($arg)*))
+    ($($arg:tt)*) => (format_args!(::rt::util::abort, $($arg)*))
 )
